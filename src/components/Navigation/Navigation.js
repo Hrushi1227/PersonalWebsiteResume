fa7b2c691add995508ad38543aa6f66c8450f5a8
Hrushi1227 <rushi.ghatol@gmail.com>
2024-06-26 16:24:00 +0530

    styling changes for burger menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,22 @@
-// src/components/Navigation.js
-
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./Navigation.css"; // Import CSS file for the Navigation component
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="nav-menu">
+      <div className="menu-icon" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+      </div>
+      <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
         <li className="nav-item">
           <NavLink to="/" exact className="nav-link" activeClassName="active">
             Home
